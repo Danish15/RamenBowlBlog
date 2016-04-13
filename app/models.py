f21c7ff17d99abe74a45f3a2688385af7e1fc996
zaighumrajput <zaighum47@gmail.com>
2016-04-13 20:37:08 +0000

    login -broken

--- app/models.py
+++ app/models.py
@@ -1,6 +1,11 @@
 from werkzeug.security import generate_password_hash, check_password_hash
 from . import db
+from flask.ext.login import UserMixin
+from . import login_manager
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class Role(db.Model):
     __tablename__ = 'roles'
@@ -27,6 +32,7 @@ class Role(db.Model):
 class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(64), unique=True, index=True)	
     username = db.Column(db.String(64), unique=True, index=True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
     password_hash = db.Column(db.String(128))
